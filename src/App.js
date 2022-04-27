c56fa6dcf3bc948fcdab7b2e6f3f0e2eb702d9bc
rossebell <Rosy24dr@outlook.com>
2022-04-27 12:56:33 -0400

    Deleted all the content in the App.css file, created the getAllSong function in the APP.js file, updated the react-dom to not say reac-dom-client in the index.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import DisplayMusic from "./Components/DisplayMusic/DisplayMusic"
 
 function App() {
+
+  const [songs,setSongs]=useState([])
+  useEffect(() => {
+    getAllSongs();
+  },[]);
+
+  async function getAllSongs(){
+    let response = await axios.get("http://127.0.0.1:8000/api/song/")
+    setSongs(response.data)
+  }
+
   return (
     <div>
       <header >
+        <DisplayMusic songs={getAllSongs}/>
       </header>
     </div>
   );
